815b94ce5bffb125670f71e275de8886c3ad4e4c
DESKTOP-OA99R3K\PC <sikirou195@gmail.com>
2025-05-11 05:12:49 +0100

    Mise a jour de portofolioViws

--- src/views/ContactListView.vue
+++ src/views/ContactListView.vue
@@ -75,7 +75,7 @@
   
   const fetchContacts = async () => {
     try {
-      const response = await axios.get('https://backendport-tsol.onrender.com');
+      const response = await axios.get('https://backendport-tsol.onrender.com/portofolio');
       contacts.value = response.data;
     } catch (error) {
       console.error("Erreur lors du chargement des contacts :", error);
@@ -84,7 +84,7 @@
   const deleteContact = async (id) => {
   if (confirm("Voulez-vous vraiment supprimer ce message ?")) {
     try {
-      await axios.delete(`'https://backendport-tsol.onrender.com'/${id}`);
+      await axios.delete(`'https://backendport-tsol.onrender.com/portofolio'/${id}`);
       contacts.value = contacts.value.filter(contact => contact.id !== id);
     } catch (error) {
       console.error("Erreur lors de la suppression :", error);
